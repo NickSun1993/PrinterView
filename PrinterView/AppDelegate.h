28d60902653fde5f20372e762abf3006d14674c1
孙锟 <Nick@imac.unidesigncn.local>
2018-10-18 13:45:20 +0800

    Initial Commit

---
+++ PrinterView/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PrinterView
+//
+//  Created by  Nick on 2018/10/18.
+//  Copyright © 2018年  Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
